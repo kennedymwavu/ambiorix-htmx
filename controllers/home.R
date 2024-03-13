b728fba7374ec090f48babbb619aabf75479c3aa
kennedymwavu <mwavukennedy@gmail.com>
2024-03-13 16:09:29 +0300

    ft: handle GET at '/'

---
+++ controllers/home.R
@@ -0,0 +1,17 @@
+box::use(
+  .. / store / home[home],
+  .. / templates / template_path[template_path]
+)
+
+#' Handle GET at '/'
+#'
+#' @export
+home_get <- \(req, res) {
+  res$render(
+    template_path("page.html"),
+    list(
+      title = "Home",
+      content = home()
+    )
+  )
+}
