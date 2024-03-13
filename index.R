90187aba78eaddedb95c599acaa275b6894e3c92
kennedymwavu <mwavukennedy@gmail.com>
2024-03-14 02:09:43 +0300

    ch: use movies router

--- index.R
+++ index.R
@@ -1,6 +1,7 @@
 box::use(
   ambiorix[Ambiorix],
   . / controllers / home[home_get],
+  . / routers / movies[movies_router = router],
   . / middleware / error_middleware[error_handler]
 )
 
@@ -9,4 +10,5 @@ Ambiorix$
   set_error(error_handler)$
   static("public", "static")$
   get("/", home_get)$
+  use(movies_router)$
   start()
