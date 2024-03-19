4db94a38f8ce1a9385040019d69ff259046ea28c
kennedymwavu <mwavukennedy@gmail.com>
2024-03-19 22:18:33 +0300

    ft: create modal dialog for adding a movie

--- modals/store/movies.R
+++ modals/store/movies.R
@@ -1,11 +1,16 @@
 box::use(
-  htmltools[tags, tagList],
   datasets[iris],
+  htmltools[tags, tagList],
+  lubridate[year, now],
+  . / text_input[text_input],
+  . / select_input[select_input],
   . / create_nav[create_nav],
   . / create_card[create_card],
   . / create_modal[create_modal],
   . / create_table[create_table],
   . / create_button[create_button],
+  . / create_rating_stars[create_rating_stars],
+  . / create_modal_trigger_btn[create_modal_trigger_btn],
   . / movie_collection[movie_collection]
 )
 
@@ -29,13 +34,57 @@ movies <- \() {
         ),
         create_table(
           data = movie_collection,
+          id = "movie_table",
           add_row_numbers = TRUE,
           wrap = FALSE
         ),
-        create_button(
-          class = "btn btn-outline-primary rounded-1",
+        create_modal_trigger_btn(
+          modal_id = "add_movie_modal",
+          class = "btn btn-outline-primary my-3",
           tags$i(class = "bi bi-plus-lg"),
           "Add a movie"
+        )
+      )
+    ),
+    create_modal(
+      id = "add_movie_modal",
+      title = "Add a movie",
+      body = tags$form(
+        `hx-target` = "#movie_table",
+        `hx-swap` = "outerHTML",
+        `hx-post` = "/movies/add_movie",
+        text_input(
+          id = "movie_name",
+          label = "Name",
+          hx_post = "/movies/validation/name"
+        ),
+        select_input(
+          id = "release_year",
+          label = "Year",
+          choices = 1888:year(now()),
+          selected = 2000L,
+          hx_post = "/movies/validation/year"
+        ),
+        select_input(
+          id = "rating",
+          label = "Rating",
+          choices = 5:1,
+          hx_post = "/movies/validation/rating"
+        ),
+        tags$div(
+          class = "w-100 d-flex justify-content-between",
+          create_button(
+            class = "btn btn-danger btn-sm",
+            `data-bs-dismiss` = "modal",
+            tags$i(class = "bi bi-x-lg"),
+            "Close"
+          ),
+          create_button(
+            type = "submit",
+            class = "btn btn-success btn-sm",
+            tags$i(class = "bi bi-check-lg"),
+            "Add movie"
+          )
         )
       )
     )
