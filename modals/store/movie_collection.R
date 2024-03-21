4d751ea4ed1d3cef67084a5e1f0fd1675fae45d9
kennedymwavu <mwavukennedy@gmail.com>
2024-03-21 16:17:26 +0300

    ch: convert movie name to titlecase

--- modals/store/movie_collection.R
+++ modals/store/movie_collection.R
@@ -1,10 +1,16 @@
+box::use(
+  stringr[str_to_title]
+)
+
 #' Movie collection
 #'
 #' @export
 movie_collection <- list(
-  Title = c(
-    "Back to the Future", "Who Framed Roger Rabbit",
-    "Phantom of the Paradise", "Back to the Future 2"
+  Title = str_to_title(
+    c(
+      "Back to the Future", "Who Framed Roger Rabbit",
+      "Phantom of the Paradise", "Back to the Future 2"
+    )
   ),
   Year = c(1985, 1988, 1974, 1989),
   Rating = c(5, 2, 4, 4)
