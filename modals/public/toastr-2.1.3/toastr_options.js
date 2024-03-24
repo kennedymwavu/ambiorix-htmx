2b6caa996126cfa154eecd9a76cf38cd1ce073e7
kennedymwavu <mwavukennedy@gmail.com>
2024-03-24 20:35:39 +0300

    ft: define toastr options

---
+++ modals/public/toastr-2.1.3/toastr_options.js
@@ -0,0 +1,17 @@
+toastr.options = {
+  closeButton: true,
+  debug: false,
+  newestOnTop: true,
+  progressBar: true,
+  positionClass: "toast-bottom-center",
+  preventDuplicates: false,
+  onclick: null,
+  showDuration: "300",
+  hideDuration: "1000",
+  timeOut: "5000",
+  extendedTimeOut: "1000",
+  showEasing: "swing",
+  hideEasing: "linear",
+  showMethod: "fadeIn",
+  hideMethod: "fadeOut",
+};
