3f252ba8bf1b5479064216cfe66627613fc9e000
kennedymwavu <mwavukennedy@gmail.com>
2024-03-25 08:32:19 +0300

    ft: add year & rating to `hx-vals` of btns

--- modals/store/create_movie_table.R
+++ modals/store/create_movie_table.R
@@ -24,8 +24,15 @@ box::use(
 #' @export
 create_movie_table <- \(..., movie_collection) {
   dt <- as.data.table(movie_collection)
+  dt[
+    ,
+    Action := create_movie_table_buttons(
+      name = Title,
+      year = Year,
+      rating = Rating
+    )
+  ]
   dt[, Rating := create_rating_stars(num_of_filled_stars = Rating)]
-  dt[, Action := create_movie_table_buttons(Title)]
 
   create_table(
     ...,
@@ -39,22 +46,29 @@ create_movie_table <- \(..., movie_collection) {
 #' Create table buttons (edit & delete)
 #'
 #' @param name Character vector. Movie name/title.
+#' @param year Integer vector. Release year of the movie.
+#' @param rating Integer vector. Rating of the movie.
 #' @examples
 #' movie_names <- c("LOTR", "Blacklist", "Avatar")
 #' create_movie_table_buttons(movie_names)
 #' @return A list containing objects of class `shiny.tag`.
 #' @export
-create_movie_table_buttons <- \(name) {
-  lapply(
-    X = name,
-    FUN = \(movie_name) {
-      hx_vals <- sprintf('{"movie_name": "%s"}', movie_name)
+create_movie_table_buttons <- \(name, year, rating) {
+  hx_vals <- sprintf(
+    '{"movie_name": "%s", "movie_year": "%d", "movie_rating": "%d"}',
+    name,
+    year,
+    rating
+  )
 
+  lapply(
+    X = hx_vals,
+    FUN = \(hx_val) {
       tags$div(
         create_button(
           class = "btn btn-outline-primary btn-sm",
           `hx-post` = "/movies/edit_movie",
-          `hx-vals` = hx_vals,
+          `hx-vals` = hx_val,
           tags$i(class = "bi bi-pencil-square"),
           "Edit"
         ),
@@ -62,7 +76,7 @@ create_movie_table_buttons <- \(name) {
           class = "btn btn-outline-danger btn-sm",
           `hx-delete` = "/movies/delete_movie",
           `hx-target` = "#movie_table",
-          `hx-vals` = hx_vals,
+          `hx-vals` = hx_val,
           `hx-swap` = "outerHTML",
           tags$i(class = "bi bi-trash"),
           "Delete"
