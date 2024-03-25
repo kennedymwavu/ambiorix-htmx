bedea4ecb7bf12df91ca52b95da636d2c31bbfab
kennedymwavu <mwavukennedy@gmail.com>
2024-03-25 07:38:52 +0300

    ft: create movie table buttons

--- modals/store/create_movie_table.R
+++ modals/store/create_movie_table.R
@@ -25,25 +25,7 @@ box::use(
 create_movie_table <- \(..., movie_collection) {
   dt <- as.data.table(movie_collection)
   dt[, Rating := create_rating_stars(num_of_filled_stars = Rating)]
-  dt[
-    ,
-    Action := replicate(
-      n = .N,
-      expr = tags$div(
-        create_button(
-          class = "btn btn-outline-primary btn-sm",
-          tags$i(class = "bi bi-pencil-square"),
-          "Edit"
-        ),
-        create_button(
-          class = "btn btn-outline-danger btn-sm",
-          tags$i(class = "bi bi-trash"),
-          "Delete"
-        )
-      ),
-      simplify = FALSE
-    )
-  ]
+  dt[, Action := create_movie_table_buttons(Title)]
 
   create_table(
     ...,
@@ -51,5 +33,41 @@ create_movie_table <- \(..., movie_collection) {
     id = "movie_table",
     add_row_numbers = TRUE,
     wrap = FALSE
+  )
+}
+
+#' Create table buttons (edit & delete)
+#'
+#' @param name Character vector. Movie name/title.
+#' @examples
+#' movie_names <- c("LOTR", "Blacklist", "Avatar")
+#' create_movie_table_buttons(movie_names)
+#' @return A list containing objects of class `shiny.tag`.
+#' @export
+create_movie_table_buttons <- \(name) {
+  lapply(
+    X = name,
+    FUN = \(movie_name) {
+      hx_vals <- sprintf('{"movie_name": "%s"}', movie_name)
+
+      tags$div(
+        create_button(
+          class = "btn btn-outline-primary btn-sm",
+          `hx-post` = "/movies/edit_movie",
+          `hx-vals` = hx_vals,
+          tags$i(class = "bi bi-pencil-square"),
+          "Edit"
+        ),
+        create_button(
+          class = "btn btn-outline-danger btn-sm",
+          `hx-delete` = "/movies/delete_movie",
+          `hx-target` = "#movie_table",
+          `hx-vals` = hx_vals,
+          `hx-swap` = "outerHTML",
+          tags$i(class = "bi bi-trash"),
+          "Delete"
+        )
+      )
+    }
   )
 }
