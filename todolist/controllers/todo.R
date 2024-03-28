c71ead7db92993c1816b1b9a0cad61ac2fd32926
kennedymwavu <mwavukennedy@gmail.com>
2024-03-28 03:07:38 +0300

    ft: [un]check todo item

--- todolist/controllers/todo.R
+++ todolist/controllers/todo.R
@@ -45,3 +45,34 @@ add_todo <- \(req, res) {
 
   res$send(html)
 }
+
+#' Handle POST at '/check_todo'
+#'
+#' PS: In case you're curious as to why I use `getElement(x, name)` in
+#' this handler, check this tweet:
+#' https://x.com/kennedymwavu/status/1773134676149817505?s=20
+#' @export
+check_todo <- \(req, res) {
+  body <- parse_multipart(req)
+  item_id <- trimws(getElement(body, "item_id") %||% "")
+  item_id2 <- trimws(getElement(body, "item_id2") %||% "")
+
+  todos <- Todo$new()
+
+  is_valid_id <- !identical(item_id, "")
+  is_valid_id2 <- !identical(item_id2, "")
+
+  # if 'item_id' is valid => checked
+  if (is_valid_id) {
+    todos$update(item_id = item_id, new_status = TRUE)
+  }
+
+  # if 'item_id' is invalid => unchecked
+  if (!is_valid_id && is_valid_id2) {
+    todos$update(item_id = item_id2, new_status = FALSE)
+  }
+
+  html <- create_todo_list(todos$read())
+
+  res$send(html)
+}
--- todolist/routers/todo.R
+++ todolist/routers/todo.R
@@ -2,7 +2,8 @@ box::use(
   ambiorix[Router],
   .. / controllers / todo[
     home_get,
-    add_todo
+    add_todo,
+    check_todo
   ]
 )
 
@@ -12,4 +13,5 @@ box::use(
 router <- Router$
   new("/")$
   get("/", home_get)$
-  post("/add_todo", add_todo)
+  post("/add_todo", add_todo)$
+  post("/check_todo", check_todo)
