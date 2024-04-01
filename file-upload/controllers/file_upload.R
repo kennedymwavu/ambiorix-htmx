2649007f55a2ee8d0340f0cf856d1cc229ce5463
kennedymwavu <mwavukennedy@gmail.com>
2024-04-01 05:33:05 +0300

    ft: handle POST on '/upload' & GET on '/data/:file_name'

--- file-upload/controllers/file_upload.R
+++ file-upload/controllers/file_upload.R
@@ -1,9 +1,16 @@
 box::use(
+  dplyr,
+  rlang[inject],
   htmltools[tags],
   data.table[fread],
   uuid[UUIDgenerate],
   webutils[parse_http],
   .. / templates / template_path[template_path],
+  .. / store / datatable[
+    datatable,
+    default_datatable_options
+  ],
+  .. / store / create_card[create_card],
   .. / store / file_upload[file_upload_page = page]
 )
 
@@ -32,21 +39,73 @@ file_upload <- \(req, res) {
   body <- req$rook.input$read()
   postdata <- parse_http(body, content_type)
 
-  file_name <- paste0(
+  file_name <- paste(
     UUIDgenerate(),
-    basename(postdata[["file"]][["filename"]])
+    basename(postdata[["file"]][["filename"]]),
+    sep = "-"
   )
 
-  # file_path <- file.path(getwd(), file_name)
-  # on.exit(unlink(file_path))
+  file_path <- file.path(getwd(), file_name)
+  writeBin(object = postdata$file$value, con = file_path)
 
-  # writeBin(object = postdata$file$value, con = file_path)
-  # uploaded <- fread(file = file_path)
-  # print(uploaded)
+  # determine the colnames:
+  col_names <- data.table::fread(file = file_path, nrows = 3L) |> colnames()
 
-  html <- tags$p(
-    class = "text-success",
-    "File uploaded successfully!"
+  html <- inject(
+    create_card(
+      class = "my-3",
+      datatable(
+        col_names = col_names,
+        table_id = "uploaded_data",
+        # send a GET request (the default for ajax) to "/data/:file_name"
+        ajax = paste0("/data/", file_name),
+        !!!default_datatable_options()
+      )
+    )
   )
+
   res$send(html)
+}
+
+#' Handle GET at '/data/:file_name'
+#'
+#' @param req Request object.
+#' @param res Response object.
+#' @export
+get_uploaded <- \(req, res) {
+  draw <- req$query$draw
+  start_row <- as.integer(req$query$start) + 1L # datatables use 0-based indexing (JS)
+  num_of_rows <- as.integer(req$query$length) - 1L
+  end_row <- start_row + num_of_rows
+
+  file_name <- req$params$file_name
+  uploaded <- data.table::fread(file = file_name)
+  search_res <- uploaded
+
+  # perform search:
+  search_value <- req$query$`search[value]`
+  if (!is.null(search_value)) {
+    found <- lapply(uploaded, \(cl) {
+      grepl(pattern = search_value, x = cl, ignore.case = TRUE)
+    }) |>
+      as.data.frame() |>
+      rowSums(na.rm = TRUE)
+    found <- which(found > 0)
+    search_res <- uploaded |> dplyr$slice(found)
+  }
+
+  records_filtered <- nrow(search_res)
+
+  # filter out the requested rows:
+  row_inds <- seq(from = start_row, to = end_row, by = 1L)
+  filtered <- search_res |> dplyr$slice(row_inds)
+
+  # datatable expects json:
+  response <- list(
+    draw = draw,
+    recordsTotal = nrow(uploaded),
+    recordsFiltered = records_filtered,
+    data = filtered
+  )
+  res$json(response)
 }
